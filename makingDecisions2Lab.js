f61d3770ec2354dba6c727cf9b188801c94264ba
William Cuthbertson <whc1315@gmail.com>
2022-01-06 15:03:15 -0700

    2 left

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -140,6 +140,9 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 
 //Code Here
 
+for (let i = letters.length - 1; i >= 0; i --){
+  console.log(letters[i]);
+}
 
 ////////// Advanced Problems //////////
 
