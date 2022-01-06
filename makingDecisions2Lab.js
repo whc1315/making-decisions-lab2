c8405750c255e6f4dd7972b53616255e000973f9
William Cuthbertson <whc1315@gmail.com>
2022-01-06 14:14:27 -0700

    6 down

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -72,7 +72,17 @@ var score = 74
 
 //Code Here
 
-
+if (score >= 90) {
+  console.log('A')
+} else if (score >= 80){
+  console.log('B')
+} else if (score >= 70){
+  console.log('C')
+} else if (score >= 60){
+  console.log('D')
+} else {
+  console.log('F')
+}
 ////////// Intermediate Problems //////////
 
 ////////// PROBLEM 6 //////////
