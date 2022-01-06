b9dafeb767a8aeb04520e7a43343597d0e1cb296
William Cuthbertson <whc1315@gmail.com>
2022-01-06 13:29:30 -0700

    three problems done

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -33,7 +33,9 @@ var family = ['Tyler', 'Jordan', 'Ryan', 'Alice', 'Ireland'];
 
 //Code Here
 
-
+for (i = 0; i < family.length; i++){
+  console.log(family[i])
+}
 
 ////////// PROBLEM 4 //////////
 
